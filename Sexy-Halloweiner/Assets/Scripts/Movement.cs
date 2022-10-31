ddc500538c80bc13a2f3dfac2b439de2401211f9
alekaixd <aleksi.pirttinen@gmail.com>
2022-10-31 13:07:28 +0200

    added movement script on player

---
+++ Sexy-Halloweiner/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public Rigidbody2D rigidbody;
+    public Vector2 movement; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
+
