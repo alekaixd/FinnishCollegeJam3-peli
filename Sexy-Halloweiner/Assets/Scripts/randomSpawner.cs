fbe381acd586d63ec8765c5c688d29b6570c107c
alekaixd <aleksi.pirttinen@gmail.com>
2022-11-03 13:30:27 +0200

    Enemies spawn once every 10 seconds

--- Sexy-Halloweiner/Assets/Scripts/randomSpawner.cs
+++ Sexy-Halloweiner/Assets/Scripts/randomSpawner.cs
@@ -6,23 +6,36 @@ public class randomSpawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject[] EnemyPrefabs;
+    public bool spawnEnemies = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnZombie());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetMouseButtonDown(0))
         {
             int randEnemy = Random.Range(0, EnemyPrefabs.Length);
             int randSpawnPoint = Random.Range(0, spawnPoints.Length);
 
             Instantiate(EnemyPrefabs[randEnemy], spawnPoints[randSpawnPoint].position, transform.rotation);
 
+        }*/
+
+    }
+
+    private IEnumerator SpawnZombie()
+    {
+        while(spawnEnemies == true)
+        {
+            yield return new WaitForSeconds(10);
+            int randEnemy = Random.Range(0, EnemyPrefabs.Length);
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+            Instantiate(EnemyPrefabs[randEnemy], spawnPoints[randSpawnPoint].position, transform.rotation);
         }
-        
+        yield return null;
     }
 }
