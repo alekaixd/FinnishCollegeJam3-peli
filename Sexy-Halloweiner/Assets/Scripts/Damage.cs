4c0f72140627553345c1e14cb256ca0f3d9e55f3
alekaixd <aleksi.pirttinen@gmail.com>
2022-11-03 20:09:36 +0200

    did sexing

--- Sexy-Halloweiner/Assets/Scripts/Damage.cs
+++ Sexy-Halloweiner/Assets/Scripts/Damage.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
+    public Component damageCollider;
+    private bool immunity = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,31 @@ public class Damage : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        GameObject gameController = GameObject.Find("GameController");
+        GameControllerScript controllScript = gameController.GetComponent<GameControllerScript>();
+        
+        Debug.Log("Hit");
+        if(immunity == false)
+        {
+            Debug.Log("damage taken");
+            controllScript.karkit -= 5;
+            StartCoroutine(ImmunityFrames());
+        }
+        else
+        {
+            Debug.Log("Immune");
+        }
+    }
+
+    private IEnumerator ImmunityFrames()
+    {
+        Debug.Log("Immunity Frames");
+        immunity = true;
+        yield return new WaitForSeconds(2);
+        immunity = false;
     }
 }
